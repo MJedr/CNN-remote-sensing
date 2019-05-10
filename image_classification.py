c398a7491d9ac4f13698459a4047b06001977486
MJedr <jedrych.marcysia@gmail.com>
2019-05-10 20:50:25 +0200

    correct image classification file

--- image_classification.py
+++ image_classification.py
@@ -1,51 +1,80 @@
 from osgeo import gdal, osr
+import numpy as np
 import pickle
 import gdal, gdalnumeric
 from gdalconst import *
 
-def classify_raster(raster_path, model_path, out_raster_name):
+def classify_tile(array, model):
+    temp = np.zeros([array.shape[0], array.shape[1]])
+    for nb, i in enumerate(array):
+        temp[nb] = model.predict(i.reshape(1, -1))
+    temp = temp.reshape(array.shape[0], array.shape[1])
+    return temp
+
+def classify_raster(raster_path, model_path, out_raster_name, x_block_size=256, y_block_size=160):
     ds = gdal.Open(raster_path, GA_ReadOnly)
     model = pickle.load(open(model_path, 'rb'))
     band = ds.GetRasterBand(1)
-    xsize = band.XSize
-    ysize = band.YSize
+    b_array = band.ReadAsArray()
+    x_im_size = band.XSize
+    y_im_size = band.YSize
+    x_block_size = int(x_block_size)
+    y_block_size = int(y_block_size)
+    xsize = b_array.shape[0]
+    ysize = b_array.shape[1]
+    xstride = np.floor(xsize / x_block_size).astype('int64')
+    ystride = np.floor(ysize / y_block_size).astype('int64')
+    max_xstride_range = int(x_block_size * xstride)
+    max_ystride_range = int(y_block_size * ystride)
+    nb_px_not_ystride = int(ysize % y_block_size)
+    nb_px_not_xstride = int(xsize % x_block_size)
+    out_raster = np.zeros([xsize, ysize])
 
     memory_driver = gdal.GetDriverByName('GTiff')
     proj = ds.GetProjectionRef()
     ext = ds.GetGeoTransform()
-    out_raster_ds = memory_driver.Create(out_raster_name, xsize, ysize, 1, gdal.GDT_UInt16)
+    out_raster_ds = memory_driver.Create(out_raster_name, x_im_size, y_im_size, 1, gdal.GDT_UInt16)
     out_raster_ds.SetProjection(proj)
     out_raster_ds.SetGeoTransform(ext)
-    out_raster = out_raster_ds.ReadAsArray()
 
-    block_sizes = ds.GetRasterBand(1).GetBlockSize()
-    x_block_size = block_sizes[0] * 10
-    y_block_size = block_sizes[1]
-    xsize = ds.GetRasterBand(1).XSize
-    ysize = ds.GetRasterBand(1).YSize
-
-    blocks = 0
     pixels = 0
-    for y in range(0, ysize, y_block_size):
-        if y + y_block_size < ysize:
-            rows = y_block_size
-        else:
-            rows = ysize - y
-        for x in range(0, xsize, x_block_size):
-            if x + x_block_size < xsize:
-                cols = x_block_size
-            else:
-                cols = xsize - x
-            array = ds.ReadAsArray(x, y, cols, rows).reshape(cols * rows, 430)
-            print(array.shape)
-            for i in array:
-                pred = model.predict(i.reshape(1, -1))
-                out_raster[x][y] = pred
-                pixels += 1
-            del array
-            blocks += 1
+    for yy in range(0, ystride):
+        for xx in range(0, xstride):
+            y = xx * x_block_size
+            x = yy * y_block_size
+            array = ds.ReadAsArray(y, x, y_block_size, x_block_size).reshape(x_block_size * y_block_size, 30)
+            out_raster[x:x + x_block_size, y:y + y_block_size] = classify_tile(array, model)
             print('classified {0} pixels out of {1}'.format(pixels, xsize * ysize))
-    ds = None
-    out_raster = None
+            print('-------------------------------------')
+        if yy == (ystride - 1):
+            if xsize % x_block_size != 0 and ysize % y_block_size != 0:
+                array = ds.ReadAsArray(y, 0, int(ysize % y_block_size), int(xsize)). \
+                    reshape((ysize % y_block_size) * xsize, 30)
+                out_raster[y_block_size * ystride: y_block_size * ystride + ysize % y_block_size, 0:xsize] = classify_tile(array, model)
+                new_x_stride = int(xsize - (xstride * x_block_size))
+                new_x = int(xstride * x_block_size)
+                new_y = 0
+                for yyy in range(0, ystride):
+                    array = ds.ReadAsArray(new_x, new_y, new_x_stride, y_block_size). \
+                        reshape(new_x_stride * y_block_size, 30)
+                    out_raster[new_y:new_y + y_block_size, new_x:xsize] = classify_tile(array)
+                    new_y += y_block_size
+            elif xsize % x_block_size != 0 and ysize % y_block_size == 0:
+                array = ds.ReadAsArray(int(xstride * x_block_size), 0, int(xsize % x_block_size), int(ysize)). \
+                    reshape((xsize % x_block_size) * ysize, 30)
+                out_raster[0:ysize, x_block_size * xstride: x_block_size * xstride + xsize % x_block_size] = \
+                    classify_tile(array, model)
+            elif xsize % x_block_size == 0 and ysize % y_block_size != 0:
+                array = ds.ReadAsArray(y, 0, int(ysize % y_block_size), int(xsize)). \
+                    reshape(nb_px_not_ystride * xsize, 30)
+                out_raster[y_block_size * ystride: y_block_size * ystride + ysize % y_block_size, 0:xsize] = \
+                    classify_tile(array, model)
+            print('classified {0} pixels out of {1}'.format(pixels, xsize * ysize))
 
-    return
+    outband = out_raster_ds.GetRasterBand(1)
+    outband.WriteArray(out_raster)
+    outband.FlushCache()
+
+    ds = None
+
+    return out_raster
